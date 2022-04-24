04b2cd00de642c388c111a53f8204a29b20c0188
Cristina Biasi s281936 <s281936@studenti.polito.it>
2022-04-24 16:32:53 +0200

    create Spring project

---
+++ lab3/src/main/kotlin/it/polito/group24/lab3/Lab3Application.kt
@@ -0,0 +1,11 @@
+package it.polito.group24.lab3
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Lab3Application
+
+fun main(args: Array<String>) {
+    runApplication<Lab3Application>(*args)
+}
---
+++ lab3/src/test/kotlin/it/polito/group24/lab3/Lab3ApplicationTests.kt
@@ -0,0 +1,13 @@
+package it.polito.group24.lab3
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Lab3ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
