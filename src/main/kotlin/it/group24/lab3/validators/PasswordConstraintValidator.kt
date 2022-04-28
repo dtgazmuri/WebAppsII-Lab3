f98182a3a25151f5bb2d513885cef404838b6f83
LUCA COSTA s290230 <s290230@studenti.polito.it>
2022-04-28 16:14:12 +0200

    All finished

--- src/main/kotlin/it/group24/lab3/validators/PasswordConstraintValidator.kt
+++ src/main/kotlin/it/group24/lab3/validators/PasswordConstraintValidator.kt
@@ -1,13 +1,11 @@
 package it.group24.lab3.validators
 
+import java.util.regex.Pattern
 import javax.validation.ConstraintValidator
 import javax.validation.ConstraintValidatorContext
 
 class PasswordConstraintValidator : ConstraintValidator<PasswordConstraint, String> {
     override fun isValid(value: String?, context: ConstraintValidatorContext?): Boolean {
-        return (value != null && value.length > 8 && value.contains(Regex("[0-9]+"))
-                && value.contains(Regex("[a-z]+")) && value.contains(Regex("[A-Z]+"))
-                && value.contains(Regex("[a-z]+")) && value.contains(Regex("[^\\w]+")))
-
+        return (value != null && Pattern.matches("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@\$%^&*-])\\S{8,}\$", value))
     }
 }
